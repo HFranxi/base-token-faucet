83e5a83624529192e321dacce970cff4a917e3a2
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-11 09:19:01 +0700

    add faucet contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,1 +1,14 @@
+pragma solidity ^0.8.20;
 
+contract Faucet {
+
+    mapping(address => uint256) public lastClaim;
+
+    function claim() public {
+
+        require(block.timestamp > lastClaim[msg.sender] + 1 hours);
+
+        lastClaim[msg.sender] = block.timestamp;
+
+    }
+}
